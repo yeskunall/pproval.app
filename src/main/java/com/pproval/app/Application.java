95c047d03deb108e6d09d9494c9161f04d1ee955
Kunall Banerjee <yeskunall96@gmail.com>
2019-03-01 00:36:54 -0500

    Rename `AppApplication` → `Application`

--- src/main/java/com/pproval/app/Application.java
+++ src/main/java/com/pproval/app/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
-
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
--- src/main/java/com/pproval/app/ServletInitializer.java
+++ src/main/java/com/pproval/app/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AppApplication.class);
+		return application.sources(Application.class);
 	}
 
 }
