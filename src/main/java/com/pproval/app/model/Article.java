7b99690539c878a2c2432ed3d38e52741334df56
Ryan Ribeiro <rmr@ryanribeiro.com>
2019-04-01 17:37:59 -0400

    Updated the Article class and the unit tests for it.

--- src/main/java/com/pproval/app/model/Article.java
+++ src/main/java/com/pproval/app/model/Article.java
@@ -2,23 +2,26 @@ package com.pproval.app.model;
 
 import javax.persistence.*;
 import java.util.Date;
+import lombok.*;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Article {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id = null;
-  String name;
-  Date submission;
-  Date deadline;
+  private String name;
+  private Date submission;
+  private Date deadline;
   @OneToOne
-  Reviewer reviewer;
+  private Reviewer reviewer;
   @OneToOne
-  Submitter submitter;
-  String review;
-  String article;
-  Status status;
+  private Submitter submitter;
+  private String review;
+  private String article;
+  private Status status;
 
   public enum Status {
     SUBMITTED,
@@ -32,80 +35,5 @@ public class Article {
     this.name = name;
     this.article = article;
     this.status = Status.SUBMITTED;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public Date getDeadline() {
-    return deadline;
-  }
-
-  public void setDeadline(Date deadline) {
-    this.deadline = deadline;
-  }
-
-  public Reviewer getReviewer() {
-    return reviewer;
-  }
-
-  public void setReviewer(Reviewer reviewer) {
-    this.reviewer = reviewer;
-  }
-
-  public String getReview() {
-    return review;
-  }
-
-  public void setReview(String review) {
-    this.review = review;
-  }
-
-  public String getContent() {
-    return article;
-  }
-
-  public void setArticle(String article) {
-    this.article = article;
-  }
-
-  public Status getStatus() {
-    return status;
-  }
-
-  public void setStatus(Status status) {
-    this.status = status;
-  }
-
-  public Submitter getSubmitter() {
-    return submitter;
-  }
-
-  public void setSubmitter(Submitter submitter) {
-    this.submitter = submitter;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Date getSubmission() {
-    return submission;
-  }
-
-  public void setSubmission(Date submission) {
-    this.submission = submission;
-  }
-
-  public Article(){
   }
 }
---
+++ src/test/java/com/pproval/app/model/ArticleTest.java
@@ -0,0 +1,78 @@
+package com.pproval.app.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArticleTest {
+
+  private Submitter submitter;
+  private Reviewer reviewer;
+  private Article article;
+
+  @Before
+  public void setUp() throws Exception {
+    submitter = new Submitter("subUsername", "subPassword");
+    reviewer = new Reviewer("revUsername", "revPassword");
+    article = new Article(submitter, "Final Project Report", "Just pretend " +
+      "this is my final report.");
+    article.setReviewer(reviewer);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void getName() {
+    assertEquals("Name should be 'Final Project Report'.", "Final Project " +
+      "Report", article.getName());
+  }
+
+  @Test
+  public void getReviewer() {
+    assertEquals("Reviewers should match.", reviewer, article.getReviewer());
+  }
+
+  @Test
+  public void getSubmitter() {
+    assertEquals("Submitters should match.", submitter,
+      article.getSubmitter());
+  }
+
+  @Test
+  public void getStatus() {
+    assertEquals("Review should be SUBMITTED.", Article.Status.SUBMITTED,
+      article.getStatus());
+  }
+
+  @Test
+  public void getArticle() {
+    assertEquals("Article text should match.", "Just pretend this is my final" +
+      " report", article.getArticle());
+  }
+
+  @Test
+  public void setName() {
+    article.setName("New Title");
+    assertEquals("Name should be 'New Title'.", "New Title",
+      article.getName());
+  }
+
+  @Test
+  public void setArticle() {
+    article.setArticle("New text.");
+    assertEquals("Article body should match.", "New text.",
+      article.getArticle());
+  }
+
+  @Test
+  public void setStatus() {
+    article.setStatus(Article.Status.ACCEPTED);
+    assertEquals("Review should be ACCEPTED.", Article.Status.ACCEPTED,
+      article.getStatus());
+  }
+}
